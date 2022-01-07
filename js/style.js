934162a1b3567f64dfe6c67a7348ca97068063e5
Raman Tamang <91386435+ramantamang777@users.noreply.github.com>
2022-01-07 16:35:35 +0530

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,7 @@
+function darkAdd(){
+    const dark = document.body;
+    dark.classList.toggle('dark-mode');
+
+    const para = document.getElementById('headingChange');
+    para.style = 'color : white;'
+}
